650f42f48a2081d3805d5674a362da82becde359
NabilSayyad <98703720+NabilSayyad@users.noreply.github.com>
2023-05-11 18:17:57 +0530

    Time: 1 ms (43.77%), Space: 43.3 MB (44.26%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -42,10 +42,8 @@ Algo :
 
 Run a while loop from 0 to last index
 
-If arr[expectedIndex] == currValue => then move to next index
-Else do a swap and stay at the same index
-
-Ensure that expectedIndex does not cross array boundaries, so before comparison do a boundary check
+If (expectedIndex in bounds && arr[expectedIndex] != currValue) => do a swap and stay at the same index
+Else move to next index
 
 TC : O(N)
 SC : O(1)
