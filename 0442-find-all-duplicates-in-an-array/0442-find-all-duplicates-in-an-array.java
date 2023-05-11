3b06a6c8fc7702f6d9ae5f3f9ce01554d6f81ca8
NabilSayyad <98703720+NabilSayyad@users.noreply.github.com>
2023-05-11 18:04:11 +0530

    Time: 6 ms (88.62%), Space: 50.8 MB (66.83%) - LeetHub

---
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.java
@@ -0,0 +1,54 @@
+/*
+range of numbers : 1,2,3....N
+indices in array : 0,1,2,3...N-1
+
+Expected index = Number - 1
+
+Apply cyclic sort
+
+Iterate and return all the numbers not at their correct positions.
+
+Algo : 
+
+Run a for loop from 0 to last index
+
+If arr[expectedIndex] == currValue => then move to next index
+Else do a swap and stay at the same index
+
+Ensure that expectedIndex does not cross array boundaries, so before comparison do a boundary check
+
+TC : O(N)
+SC : O(1)
+
+
+*/
+
+class Solution {
+    public List<Integer> findDuplicates(int[] nums) {
+        List<Integer> disappearedNumbers = new ArrayList<>();
+        int i=0;
+        int expectedIndex;
+        while(i < nums.length){
+            int currValue = nums[i];
+            expectedIndex = currValue - 1;
+            if(currValue != nums[expectedIndex]){
+                swap(nums, i, expectedIndex);
+            }
+            else{
+                i++;
+            }
+        }
+        for(i=0; i<nums.length; i++){
+            if(nums[i] != i+1){
+                disappearedNumbers.add(nums[i]);
+            }
+        }
+        return disappearedNumbers;
+    }
+    
+    private static void swap(int[]nums, int i, int expectedIndex){
+        int temp = nums[i];
+        nums[i] = nums[expectedIndex];
+        nums[expectedIndex] = temp;
+    }
+}
