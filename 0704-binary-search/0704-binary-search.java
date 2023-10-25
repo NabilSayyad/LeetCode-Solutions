367e6a5c32fa13cfd9b97dfa91813469739f3d08
NabilSayyad <98703720+NabilSayyad@users.noreply.github.com>
2023-10-26 00:23:29 +0530

    Time: 0 ms (100.00%), Space: 44.5 MB (77.70%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -1,30 +1,88 @@
+/*
+Standard binary search question
+
+Key points : 
+1) Integer overflow should be avoided
+2) While should always have low <= high instead of low < high
+3) 3 cases so use else if properly
+4) if not found, return -1
+
+*/
+/*
 class Solution {
     public int search(int[] nums, int target) {
         int low = 0;
         int high = nums.length - 1;
-        boolean isAsc = nums[low] < nums[high];
         while(low <= high){
+            // Calculate middle element
             int mid = low + (high-low)/2;
+            // Check for the 3 cases
             if(nums[mid] == target){
+                // I have found the answer, so return its index
                 return mid;
             }
             else if(nums[mid] > target){
-                if(isAsc){
+                // My target lies to the left
+                // Eliminate the right portion
+                high = mid - 1;
+            }
+            else{
+                // My target lies to the right
+                // Eliminate the left portion
+                low = mid + 1;
+            }
+        }
+        // I didnt return anything till now, means
+        // I didnt find my target
+        // So I return -1 as per problem statement
+        return -1;
+    }
+}
+*/
+
+// Order agnostic binary search
+
+class Solution {
+    public int search(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+        boolean ascSorted = nums[low] < nums[high];
+        while(low <= high){
+            // Calculate middle element
+            int mid = low + (high-low)/2;
+            // Check for the 3 cases
+            if(nums[mid] == target){
+                // I have found the answer, so return its index
+                return mid;
+            }
+            else if(nums[mid] > target){
+                if(ascSorted){
+                    // My target lies to the left
+                    // Eliminate the right portion
                     high = mid - 1;
                 }
                 else{
+                    // My target lies to the right
+                    // Eliminate the left portion
                     low = mid + 1;
                 }
             }
             else{
-                if(isAsc){
+                if(ascSorted){
+                    // My target lies to the right
+                    // Eliminate the left portion
                     low = mid + 1;
                 }
                 else{
+                    // My target lies to the left
+                    // Eliminate the right portion
                     high = mid - 1;
                 }
             }
         }
+        // I didnt return anything till now, means
+        // I didnt find my target
+        // So I return -1 as per problem statement
         return -1;
     }
 }
