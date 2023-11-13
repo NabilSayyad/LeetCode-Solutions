b88175b3b1b41bad9d117a0b417eed1d7202e88c
NabilSayyad <98703720+NabilSayyad@users.noreply.github.com>
2023-11-14 00:13:50 +0530

    Time: 1 ms (70.19%), Space: 40.7 MB (95.26%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -1,30 +1,26 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+
+/*
+
+Same as level order traversal with only difference being that only the last one from each level gets to be part of the answer.
+
+So at the end of each for loop, just add the last element removed from the queue to the answer.
+
+TC : O(N)
+SC : O(N)
+*/
+
 class Solution {
     public List<Integer> rightSideView(TreeNode root) {
-        List<Integer> rightSideView = new ArrayList<>();
-        if(root == null){
-            return rightSideView;
-        }
+        List<Integer> ans = new ArrayList<>();
         Queue<TreeNode> queue = new LinkedList<>();
+        if(root == null){
+            return ans;
+        }
         queue.add(root);
-        TreeNode curr = null;
         while(!queue.isEmpty()){
             int currLevelSize = queue.size();
-            for(int i = 0; i < currLevelSize; i++){
+            TreeNode curr = null;
+            while(currLevelSize-- > 0){
                 curr = queue.remove();
                 if(curr.left != null){
                     queue.add(curr.left);
@@ -33,8 +29,8 @@ class Solution {
                     queue.add(curr.right);
                 }
             }
-            rightSideView.add(curr.val);
+            ans.add(curr.val);
         }
-        return rightSideView;
+        return ans;
     }
 }
