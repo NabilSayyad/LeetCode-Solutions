d33e40780c9fddb4fe12bb07ebd0ba6d0b846867
NabilSayyad <98703720+NabilSayyad@users.noreply.github.com>
2023-11-13 23:18:03 +0530

    Time: 1 ms (94.59%), Space: 42.3 MB (10.89%) - LeetHub

---
+++ 0107-binary-tree-level-order-traversal-ii/0107-binary-tree-level-order-traversal-ii.java
@@ -0,0 +1,74 @@
+/*
+Approach 1 : 
+-> The code is as it is except that we need to return the reverse of what we get in level order traversal.
+
+-> So we need to reverse the level order traversal before returning.
+
+Collections.reverse(levelOrderTraversal);
+
+Approach : If we do not want to reverse it, then simply use add(index, value) and at every level, append the currLevelList at index 0
+
+*/
+
+/*
+
+Approach 1 : 
+
+class Solution {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> levelOrderTraversal = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        if(root == null){
+            return levelOrderTraversal;
+        }
+        queue.add(root);
+        while(!queue.isEmpty()){
+            List<Integer> currLevelList = new LinkedList<>();
+            int currLevelSize = queue.size();
+            while(currLevelSize-- > 0){
+                TreeNode curr = queue.remove();
+                if(curr.left != null){
+                    queue.add(curr.left);
+                }
+                if(curr.right != null){
+                    queue.add(curr.right);
+                }
+                currLevelList.add(curr.val);
+            }
+            levelOrderTraversal.add(currLevelList);
+        }
+        Collections.reverse(levelOrderTraversal);
+        return levelOrderTraversal;
+    }
+}
+
+*/
+
+/*Approach 2 */
+
+class Solution {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> levelOrderTraversal = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        if(root == null){
+            return levelOrderTraversal;
+        }
+        queue.add(root);
+        while(!queue.isEmpty()){
+            List<Integer> currLevelList = new LinkedList<>();
+            int currLevelSize = queue.size();
+            while(currLevelSize-- > 0){
+                TreeNode curr = queue.remove();
+                if(curr.left != null){
+                    queue.add(curr.left);
+                }
+                if(curr.right != null){
+                    queue.add(curr.right);
+                }
+                currLevelList.add(curr.val);
+            }
+            levelOrderTraversal.add(0, currLevelList);
+        }
+        return levelOrderTraversal;
+    }
+}
